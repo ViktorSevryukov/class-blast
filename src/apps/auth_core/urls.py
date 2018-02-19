45c253efa5e4302d5c3b26ec735494310561264d
Artyom Ernekyan <artem.e@nicecode.biz>
2018-02-19 16:32:16 +0300

    Logout button

--- src/apps/auth_core/urls.py
+++ src/apps/auth_core/urls.py
@@ -3,4 +3,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
 ]
