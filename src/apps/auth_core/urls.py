91646a5d062a2d1aa6349d336729e461c0fe101d
Artyom Ernekyan <artem.e@nicecode.biz>
2018-02-20 16:24:08 +0300

    Login redirect if service data is typed

--- src/apps/auth_core/urls.py
+++ src/apps/auth_core/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+from apps.auth_core.views import LoginView
+
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
 ]
