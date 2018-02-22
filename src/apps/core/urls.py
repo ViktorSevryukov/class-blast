8a7851d4d27f2d8a05d8b5c80b333bad7bf2fd88
Artyom Ernekyan <artem.e@nicecode.biz>
2018-02-22 16:14:25 +0300

    export API

--- src/apps/core/urls.py
+++ src/apps/core/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from apps.core.views import ServicesLoginView, DashboardView, SyncView
+from apps.core.api import export_group
 
 urlpatterns = [
     url(r'^manage/', DashboardView.as_view(), name='manage'),
     url(r'^services_login/$', ServicesLoginView.as_view(), name='services_login'),
-    url(r'^sync/$', SyncView.as_view(), name='sync')
+    url(r'^sync/$', SyncView.as_view(), name='sync'),
+    url(r'^export/$', export_group, name='export')
 ]
