37bf612cf3211e193ff01df64cec899de0b717b7
Viktor Sevryukov <viktor.s@nicecode.biz>
2018-03-07 15:04:36 +0300

    fix non-uniqe input ids on services login page

--- src/apps/core/forms.py
+++ src/apps/core/forms.py
@@ -2,10 +2,16 @@ from django import forms
 
 
 class EnrollLoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=50)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label='Username', max_length=50,
+                               widget=forms.TextInput(
+                                   attrs={'id': 'enroll_username'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'id': 'enroll_password'}))
 
 
 class AHALoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=50)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label='Username', max_length=50,
+                               widget=forms.TextInput(
+                                   attrs={'id': 'aha_username'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'id': 'aha_password'}))
