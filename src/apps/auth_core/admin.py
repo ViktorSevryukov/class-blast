bdada5a8d07f9f0306c85b09618d25f61e37cda4
Viktor Sevryukov <viktor.s@nicecode.biz>
2018-03-14 12:30:47 +0300

    custom userAdmin form * use default class

--- src/apps/auth_core/admin.py
+++ src/apps/auth_core/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _
+
 
 from apps.auth_core.models import User
 
 
-class AdminUser(admin.ModelAdmin):
-    fields = ('username', 'password', 'version', 'first_name', 'last_name', 'email',
-              'is_superuser', 'is_staff', 'is_active', 'user_permissions', 'groups',
-              'last_login', 'date_joined')
-    list_display = ('__str__', 'version')
-    search_fields = ('username', 'first_name', 'last_name')
+class UserAdmin(BaseUserAdmin):
 
-admin.site.register(User, AdminUser)
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'version')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'version', 'is_staff')
+
+admin.site.register(User, UserAdmin)
+
