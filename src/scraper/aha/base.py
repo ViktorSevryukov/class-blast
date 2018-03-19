a086022e784c1f3a5a5d65387650addf6cfda693
Viktor Sevryukov <viktor.s@nicecode.biz>
2018-03-19 14:24:13 +0300

    wait before paste course

--- src/scraper/aha/base.py
+++ src/scraper/aha/base.py
@@ -67,7 +67,7 @@ class AHABase():
 
     def go_to_add_class_page(self):
         logger.info("Go to class page")
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(5)
         WebDriverWait(self.browser, 5).until(
             EC.text_to_be_present_in_element((By.TAG_NAME, "strong"), "Welcome!"))
         self.browser.get(self.URLS['add_class'])
