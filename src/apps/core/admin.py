bdac35cd2d05eba4d4bfe72f1f88ce35478e8e2a
Artyom Ernekyan <artem.e@nicecode.biz>
2018-05-11 17:49:59 +0300

    Admin Export CSV

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -4,6 +4,7 @@ from rangefilter.filter import DateRangeFilter
 from apps.core.filters import RelatedDropdownFilter
 from .models import EnrollWareGroup, EnrollClassTime, EnrollWareCredentials, \
     AHACredentials, Mapper, AHAField, AHAGroup
+from apps.core.actions import export_as_csv_action
 
 admin.site.register(EnrollClassTime)
 admin.site.register(EnrollWareCredentials)
@@ -21,6 +22,18 @@ class AdminEnrollWareGroup(admin.ModelAdmin):
     search_fields = ('course', 'status', 'group_id', 'user',)
     list_filter = ('status', ('user', RelatedDropdownFilter),
                    ('sync_date', DateRangeFilter),)
+    actions = [export_as_csv_action("CSV Export", fields=['user',
+                                                          'group_id',
+                                                          'course',
+                                                          'location',
+                                                          'instructor',
+                                                          'max_students',
+                                                          'status',
+                                                          'available_to_export',
+                                                          'sync_date',
+                                                          'get_class_time']
+                                    )
+               ]
 
 admin.site.register(EnrollWareGroup, AdminEnrollWareGroup)
 
