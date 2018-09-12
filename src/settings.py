567328d44655f893521ca9cda5ff63d6efcf827c
Viktor Sevryukov <viktor.s@nicecode.biz>
2018-09-12 17:34:26 +0300

    fix settings

--- src/settings.py
+++ src/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blast',
+        'NAME': 'class-blast',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
